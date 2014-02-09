52b863e011e4cf729dac3eae0d1b2799799365fd
Simon Wilkinson <simon@sxw.org.uk>
2014-02-09 16:09:48 +0000

    GSSAPI key exchange support
    This patch has been rejected upstream: "None of the OpenSSH developers are
    in favour of adding this, and this situation has not changed for several
    years.  This is not a slight on Simon's patch, which is of fine quality, but
    just that a) we don't trust GSSAPI implementations that much and b) we don't
    like adding new KEX since they are pre-auth attack surface.  This one is
    particularly scary, since it requires hooks out to typically root-owned
    system resources."
    
    However, quite a lot of people rely on this in Debian, and it's better to
    have it merged into the main openssh package rather than having separate
    -krb5 packages (as we used to have).  It seems to have a generally good
    security history.
    
    Author: Simon Wilkinson <simon@sxw.org.uk>
    Author: Colin Watson <cjwatson@debian.org>
    Author: Jakub Jelen <jjelen@redhat.com>
    Origin: other, https://github.com/openssh-gsskex/openssh-gsskex/pull/23
    Bug: https://bugzilla.mindrot.org/show_bug.cgi?id=1242
    Last-Updated: 2026-03-27
    
    Patch-Name: gssapi.patch

--- kex.c
+++ kex.c
@@ -62,6 +62,7 @@
 #include "dispatch.h"
 #include "monitor.h"
 #include "myproposal.h"
+#include "xmalloc.h"
 
 #include "ssherr.h"
 #include "sshbuf.h"
@@ -747,6 +748,9 @@ kex_free(struct kex *kex)
 	sshbuf_free(kex->session_id);
 	sshbuf_free(kex->initial_sig);
 	sshkey_free(kex->initial_hostkey);
+#ifdef GSSAPI
+	free(kex->gss_host);
+#endif /* GSSAPI */
 	free(kex->failed_choice);
 	free(kex->hostkey_alg);
 	free(kex->name);
