d4c39bdded2e9d4064c4e1fc63ba8f0329fbca3d
Manoj Ampalam <manojampalam@live.com>
2016-12-05 22:55:10 -0800

    Cleanup

--- contrib/win32/win32compat/misc.c
+++ contrib/win32/win32compat/misc.c
@@ -293,3 +293,44 @@ int w32_ioctl(int d, int request, ...) {
                 return -1;
         }
 }
+
+
+int 
+spawn_child(char* cmd, int in, int out, int err, int inherit_console) {
+	PROCESS_INFORMATION pi;
+	STARTUPINFOW si;
+	BOOL b;
+	DWORD flags = (inherit_console == 1) ? 0 : DETACHED_PROCESS;
+	wchar_t * cmd_utf16;
+
+	if ((cmd_utf16 = utf8_to_utf16(cmd)) == NULL) {
+		errno = ENOMEM;
+		return -1;
+	}
+
+	memset(&si, 0, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+	si.hStdInput = w32_fd_to_handle(in);
+	si.hStdOutput = w32_fd_to_handle(out);
+	si.hStdError = w32_fd_to_handle(err);
+	si.wShowWindow = SW_HIDE;
+	si.dwFlags = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
+
+	b = CreateProcessW(NULL, cmd_utf16, NULL, NULL, TRUE, flags, NULL, NULL, &si, &pi);
+
+	if (b) {
+		if (sw_add_child(pi.hProcess, pi.dwProcessId) == -1) {
+			TerminateProcess(pi.hProcess, 0);
+			CloseHandle(pi.hProcess);
+			pi.dwProcessId = -1;
+		}
+		CloseHandle(pi.hThread);
+	}
+	else {
+		errno = GetLastError();
+		pi.dwProcessId = -1;
+	}
+
+	free(cmd_utf16);
+	return pi.dwProcessId;
+}
