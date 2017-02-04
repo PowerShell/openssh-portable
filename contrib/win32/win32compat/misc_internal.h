b230ae79eaf17a9ede5cd48355c9fb8b13fab79d
remittor <remittor@gmail.com>
2017-02-04 09:55:01 +0300

    win32: Fix execute SSHD child process from "Program Files" directory

--- contrib/win32/win32compat/misc_internal.h
+++ contrib/win32/win32compat/misc_internal.h
@@ -1,5 +1,12 @@
 #define PATH_MAX MAX_PATH
 
+#define GOTO_CLEANUP_IF(_cond_,_err_) do {  \
+    if ((_cond_)) {                         \
+        hr = _err_;                         \
+        goto cleanup;                       \
+    }                                       \
+} while(0)
+
 /* removes first '/' for Windows paths that are unix styled. Ex: /c:/ab.cd */
 char * sanitized_path(const char *);
 
