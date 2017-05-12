0aeaf9d857fad1bc1fae7e6b166b2ef1fe9ec0e1
bagajjal <bagajjal@microsoft.com>
2017-05-11 18:29:23 -0700

    added new unit testcases

---
+++ regress/unittests/win32compat/tests.h
@@ -0,0 +1,13 @@
+#pragma once
+void socket_tests();
+void file_tests();
+void miscellaneous_tests();
+
+char *dup_str(char *inStr);
+void delete_dir_recursive(char *full_dir_path);
+
+#define ASSERT_HANDLE(handle,retValue) \
+{ \
+	retValue = (handle != INVALID_HANDLE_VALUE) ? 0 : -1; \
+	ASSERT_INT_NE(retValue, -1); \
+}
