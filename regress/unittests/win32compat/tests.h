7243ec5a55f815131bc7f4c4c430cce7bee7e753
Manoj Ampalam <manojampalam@live.com>
2017-05-22 22:23:04 -0700

    Add explicit Asserts on ASSERT_HANDLE
    This will result in a proper error message thrown when assert fails.

--- regress/unittests/win32compat/tests.h
+++ regress/unittests/win32compat/tests.h
@@ -8,6 +8,6 @@ void delete_dir_recursive(char *full_dir_path);
 
 #define ASSERT_HANDLE(handle,retValue) \
 { \
-	retValue = ((handle != INVALID_HANDLE_VALUE) || (handle != NULL)) ? 0 : -1; \
-	ASSERT_INT_NE(retValue, -1); \
+	ASSERT_PTR_NE(handle, INVALID_HANDLE_VALUE); \
+	ASSERT_PTR_NE(handle, 0); \
 }
