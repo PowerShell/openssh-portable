ce5d02a6df9cec7ca2eecc1e42389190c602e453
bingbing8 <yawang@microsoft.com>
2017-05-31 19:52:18 -0700

    Add OpenSSHCheckHostConfig.ps1 in package

--- contrib/win32/openssh/OpenSSHCheckHostConfig.ps1
+++ contrib/win32/openssh/OpenSSHCheckHostConfig.ps1
@@ -1,4 +1,15 @@
-﻿#only validate owner and ACEs of the file
+﻿$systemAccount = New-Object System.Security.Principal.NTAccount("NT AUTHORITY", "SYSTEM")
+$adminsAccount = New-Object System.Security.Principal.NTAccount("BUILTIN","Administrators")            
+$currentUser = New-Object System.Security.Principal.NTAccount($($env:USERDOMAIN), $($env:USERNAME))
+$everyone =  New-Object System.Security.Principal.NTAccount("EveryOne")
+$sshdAccount = New-Object System.Security.Principal.NTAccount("NT SERVICE","sshd")
+
+<#
+    .Synopsis
+    Check-FileSecureInternal
+    Only validate owner and ACEs of the file
+#>
+
 function Check-FileSecure
 {
     param (
@@ -6,41 +17,47 @@ function Check-FileSecure
         [string]$FilePath,
         [ValidateNotNull()]
         [System.Security.Principal.NTAccount[]] $Owners = $currentUser,
-        [System.Security.Principal.NTAccount[]] $Identities,
-        [System.Security.Principal.NTAccount[]] $ReadOnlyPermIdentities
+        [System.Security.Principal.NTAccount[]] $AnyAccessOK,
+        [System.Security.Principal.NTAccount[]] $ReadAccessOK,
+        [System.Security.Principal.NTAccount[]] $ReadAccessNeeded
     )
-
-    Write-host "-----Start validating the health of file $FilePath-----"
-    $return = Check-FileSecureHelp @PSBoundParameters
+    
+    Write-host "----------Validating $FilePath----------"
+    $return = Check-FileSecureInternal @PSBoundParameters
 
     if($return -contains $true) 
     {
-        Write-host "Re-check the health of file $FilePath"
-        Check-FileSecureHelp @PSBoundParameters
+        #Write-host "Re-check the health of file $FilePath"
+        Check-FileSecureInternal @PSBoundParameters
     }
 }
 
-function Check-FileSecureHelp {
+<#
+    .Synopsis
+    Check-FileSecureInternal
+#>
+function Check-FileSecureInternal {
     param (
         [parameter(Mandatory=$true)]
         [string]$FilePath,
         [ValidateNotNull()]
         [System.Security.Principal.NTAccount[]] $Owners = $currentUser,
-        [System.Security.Principal.NTAccount[]] $Identities,
-        [System.Security.Principal.NTAccount[]] $ReadOnlyPermIdentities
+        [System.Security.Principal.NTAccount[]] $AnyAccessOK,
+        [System.Security.Principal.NTAccount[]] $ReadAccessOK,
+        [System.Security.Principal.NTAccount[]] $ReadAccessNeeded
     )
 
     $acl = Get-Acl $FilePath
     $needChange = $false
-    $hasSSHD = $false
     $health = $true
     
     if([System.Security.Principal.NTAccount]$($acl.Owner) -notin $Owners)
     {
-        $warning = "It is recommendated to have '$($Owners[0].Value)' as file owner of $FilePath."        
+        #todo: unrestrict mode
+        $warning = "Current owner: '$($acl.Owner)'. '$($Owners[0])' should own $FilePath."
         Do {
             Write-Warning $warning
-            $input = Read-Host -Prompt "Do you want to set the file owner? [Yes] Y; [No] N (default is `"Y`")"
+            $input = Read-Host -Prompt "Shall I set the file owner? [Yes] Y; [No] N (default is `"Y`")"
             if([string]::IsNullOrEmpty($input))
             {
                 $input = 'Y'
@@ -49,47 +66,52 @@ function Check-FileSecureHelp {
         
 
         if($Matches[0].ToLower().Startswith('y'))
-        {            
+        {
+            $needChange = $true
             $acl.SetOwner($Owners[0])
-            Write-Host "Set owner of file $FilePath to '$($Owners[0].Value)'. "  -ForegroundColor Green
-            Set-Acl -Path $FilePath -AclObject $acl
+            Write-Host "'$($Owners[0])' now owns $FilePath. " -ForegroundColor Green
         }
         else
         {
             $health = $false
-            Write-Host "User decided not to make the changes."
+            Write-Host "The owner is still set to '$($acl.Owner)'." -ForegroundColor Yellow
         }
     }
 
-    $ReadAccess = ([System.UInt32] [System.Security.AccessControl.FileSystemRights]::Read.value__) -bor `
+    $ReadAccessPerm = ([System.UInt32] [System.Security.AccessControl.FileSystemRights]::Read.value__) -bor `
                     ([System.UInt32] [System.Security.AccessControl.FileSystemRights]::Synchronize.value__)
+    $realAnyAccessOKList = $AnyAccessOK + @($systemAccount, $adminsAccount)
+    $realReadAcessOKList = $ReadAccessOK + $ReadAccessNeeded
+    $realReadAccessNeeded = $ReadAccessNeeded
 
     foreach($a in $acl.Access)
     {
-        if($a.IdentityReference.Value -in ($Identities | % { $_.Value } ))
+        if(($realAnyAccessOKList -ne $null) -and $realAnyAccessOKList.Contains($a.IdentityReference))
         {
             #ingore identities
         }
-        elseif(($ReadOnlyPermIdentities -and ($ReadOnlyPermIdentities.Contains($everyone))) -or `
-             ($a.IdentityReference.Value -in $ReadOnlyPermIdentities))
+        elseif($realReadAcessOKList -and (($realReadAcessOKList.Contains($everyone)) -or `
+             ($realReadAcessOKList.Contains($a.IdentityReference))))
         {
-            if($a.IdentityReference.Value -eq $sshdAccount.Value)
+            if($realReadAccessNeeded -and ($a.IdentityReference.Equals($everyone)))
             {
-                    $hasSSHD = $true;
+                $realReadAccessNeeded.Clear()
             }
+            elseif($realReadAccessNeeded -and $realReadAccessNeeded.Contains($a.IdentityReference))
+            {
+                    $realReadAccessNeeded = $realReadAccessNeeded | ? { -not $_.Equals($a.IdentityReference) }
+            }
+
             if (-not ($a.AccessControlType.Equals([System.Security.AccessControl.AccessControlType]::Allow)) -or `
-            (-not (([System.UInt32]$a.FileSystemRights.value__) -band (-bnot $ReadAccess))))
+            (-not (([System.UInt32]$a.FileSystemRights.value__) -band (-bnot $ReadAccessPerm))))
             {
                 continue;
             }
-            $warning = @"
-It is required that '$($a.IdentityReference.Value)' has Read only access on file $FilePath.
-but '$($a.IdentityReference.Value)' is granted '$($a.FileSystemRights)'.
-"@            
+            $warning = "'$($a.IdentityReference)' has the following access to $($FilePath): '$($a.FileSystemRights)'."            
 
             Do {
                     Write-Warning $warning
-                    $input = Read-Host -Prompt "Do you want to update the file permission? [Yes] Y; [No] N (default is `"Y`")"
+                    $input = Read-Host -Prompt "Shall I make it Read only? [Yes] Y; [No] N (default is `"Y`")"
                     if([string]::IsNullOrEmpty($input))
                     {
                         $input = 'Y'
@@ -98,31 +120,79 @@ but '$($a.IdentityReference.Value)' is granted '$($a.FileSystemRights)'.
                 } until ($input -match "^(y(es)?|N(o)?)$")
 
             if($Matches[0].ToLower().Startswith('y'))
-            {
-                $needChange = $true
+            {                
                 if($a.IsInherited)
                 {
+                    if($needChange)    
+                    {
+                        Set-Acl -Path $FilePath -AclObject $acl     
+                    }
                     Remove-RuleProtection -FilePath $FilePath
                     #after the inheritance is remove, you need to get acl again for correct data
                     #otherwise, the inheriance is set back to $true from $acl.
                     return $true
                 }
+                $needChange = $true
                 $sshAce = New-Object System.Security.AccessControl.FileSystemAccessRule `
-                    ($($a.IdentityReference.Value), "Read", "None", "None", "Allow")
+                    ($a.IdentityReference, "Read", "None", "None", "Allow")
                 $acl.SetAccessRule($sshAce)
-                Write-Host "Updated $($a.IdentityReference.Value) with Read access on file $FilePath. "  -ForegroundColor Green
+                Write-Host "'$($a.IdentityReference)' now has Read access to $FilePath. "  -ForegroundColor Green
             }
             else
             {
                 $health = $false
-                Write-Host "User decided not to make this changes." -ForegroundColor DarkYellow
+                Write-Host "'$($a.IdentityReference)' still has these access to $($FilePath): '$($a.FileSystemRights)'." -ForegroundColor Yellow
             }            
           }
         elseif($a.AccessControlType.Equals([System.Security.AccessControl.AccessControlType]::Allow))
         {
             Do {
-                Write-Warning "Any access of $($a.IdentityReference.Value) on '$FilePath' is not allowed. " 
-                $input = Read-Host -Prompt "Do you want to remove this access? [Yes] Y; [No] N (default is `"Y`")"
+            #todo for unstrict mode
+                Write-Warning "'$($a.IdentityReference)' cannot have access to '$FilePath'. " 
+                $input = Read-Host -Prompt "Shall I remove this access? [Yes] Y; [No] N (default is `"Y`")"
+                if([string]::IsNullOrEmpty($input))
+                {
+                    $input = 'Y'
+                }        
+            } until ($input -match "^(y(es)?|N(o)?)$")
+        
+            if($Matches[0].ToLower().Startswith('y'))
+            {                
+                if($a.IsInherited)
+                {
+                    if($needChange)    
+                    {
+                        Set-Acl -Path $FilePath -AclObject $acl     
+                    }
+                    Remove-RuleProtection -FilePath $FilePath
+                    return $true
+                }
+                $needChange = $true
+                if(-not ($acl.RemoveAccessRule($a)))
+                {
+                    throw "failed to remove access of $($a.IdentityReference) rule to file $FilePath"
+                }
+                else
+                {
+                    Write-Host "'$($a.IdentityReference)' has no more access to $FilePath." -ForegroundColor Green
+                }
+            }
+            else
+            {
+                $health = $false
+                Write-Host "'$($a.IdentityReference)' still has access to $FilePath." -ForegroundColor Yellow
+            }
+        }    
+    }
+
+    if($realReadAccessNeeded)
+    {
+        $realReadAccessNeeded | % {
+            
+            $warning = "'$_' needs Read access to $FilePath'."
+            Do {
+                Write-Warning $warning
+                $input = Read-Host -Prompt "Shall I make the above change? [Yes] Y; [No] N (default is `"Y`")"
                 if([string]::IsNullOrEmpty($input))
                 {
                     $input = 'Y'
@@ -132,52 +202,16 @@ but '$($a.IdentityReference.Value)' is granted '$($a.FileSystemRights)'.
             if($Matches[0].ToLower().Startswith('y'))
             {
                 $needChange = $true
-                if($a.IsInherited)
-                {
-                    Remove-RuleProtection -FilePath $FilePath
-                    return $true
-                }
-                if(-not ($acl.RemoveAccessRule($a)))
-                {
-                    throw "failed to remove access of $($a.IdentityReference.Value) rule to file $FilePath"
-                }
-                else
-                {
-                    Write-host "The access of $($a.IdentityReference.Value) is removed from file $FilePath." -ForegroundColor Green
-                }
+                $ace = New-Object System.Security.AccessControl.FileSystemAccessRule `
+                        ($_, "Read", "None", "None", "Allow")
+                $acl.AddAccessRule($ace)
+                Write-Host "'$_' now has Read access to $FilePath. " -ForegroundColor Green
             }
             else
             {
                 $health = $false
-                Write-Host "User decided not to make the changes."  -ForegroundColor DarkYellow
+                Write-Host "'$_' does not have Read access to $FilePath." -ForegroundColor Yellow
             }
-        }    
-    }
-
-    if($ReadOnlyPermIdentities -and (-not $hasSSHD) -and ($ReadOnlyPermIdentities.Contains($sshdAccount)))
-    {
-        $warning = "It is recommendated to grant '$sshdAccount' Read access on $FilePath'."
-        Do {
-            Write-Warning $warning
-            $input = Read-Host -Prompt "Do you want to continue with the above changes? [Yes] Y; [No] N (default is `"Y`")"
-            if([string]::IsNullOrEmpty($input))
-            {
-                $input = 'Y'
-            }        
-        } until ($input -match "^(y(es)?|N(o)?)$")
-        
-        if($Matches[0].ToLower().Startswith('y'))
-        {
-            $needChange = $true
-            $sshdAce = New-Object System.Security.AccessControl.FileSystemAccessRule `
-                    ($sshdAccount, "Read", "None", "None", "Allow")
-            $acl.AddAccessRule($sshdAce)
-            Write-Host "Added $sshdAccount with Read access on file $FilePath. "  -ForegroundColor Green
-        }
-        else
-        {
-            $health = $false
-            Write-Host "User decided not to make the changes."  -ForegroundColor DarkYellow
         }
     }
 
@@ -187,14 +221,15 @@ but '$($a.IdentityReference.Value)' is granted '$($a.FileSystemRights)'.
     }
     if($health)
     {
-        Write-Host "-------------File $FilePath is healthy!!!--------------" -ForegroundColor Green
+        Write-Host "-----------$FilePath looks good!-------- "  -ForegroundColor Green
     }
-    else
-    {
-        Write-Host "------------File $FilePath is unhealthy!!!-------------" -ForegroundColor Red
-    }
+    Write-host " "
 }
 
+<#
+    .Synopsis
+    Remove-RuleProtection
+#>
 function Remove-RuleProtection
 {
     param (
@@ -204,7 +239,6 @@ function Remove-RuleProtection
     $acl = Get-ACL $FilePath
     $acl.SetAccessRuleProtection($True, $True)
     Set-Acl -Path $FilePath -AclObject $acl
-    Write-Host "The inheritance on file $FilePath is removed."
 }
 <#
     .Synopsis
@@ -227,38 +261,74 @@ function Get-UserSID
 }
 
 
-$systemAccount = New-Object System.Security.Principal.NTAccount("NT AUTHORITY", "SYSTEM")
-$adminsAccount = New-Object System.Security.Principal.NTAccount("BUILTIN","Administrators")            
-$currentUser = New-Object System.Security.Principal.NTAccount($($env:USERDOMAIN), $($env:USERNAME))
-$everyone =  New-Object System.Security.Principal.NTAccount("EveryOne")
-$sshdAccount = New-Object System.Security.Principal.NTAccount("NT SERVICE","sshd")
-
 #check sshd config file
-Check-FileSecure -FilePath ( join-path $PSScriptRoot "sshd_config") -Owners $systemAccount -Identities $adminsAccount,$systemAccount, $currentUser -ReadOnlyPermIdentities $sshdAccount
+Check-FileSecure -FilePath ( join-path $PSScriptRoot "sshd_config") -Owners $systemAccount,$adminsAccount -ReadAccessNeeded $sshdAccount
+ 
+#check private host keys
+$insecureConfig = $false
+Do
+{                
+    $input = Read-Host -Prompt "Did you register host private keys with ssh-agent? [Yes] Y; [No] N"    
+} until ($input -match "^(y(es)?|N(o)?)$")
 
-#check private host keys    
-Get-ChildItem $PSScriptRoot\ssh*host*key* -Exclude *.pub | % {
-    Check-FileSecure -FilePath $_.FullName -Owners $systemAccount -Identities $adminsAccount,$systemAccount
+if($Matches[0].ToLower().Startswith('n')) {
+    $warning = @"
+To keep the host private keys secure, it is recommended to register them with ssh-agent following
+steps in link 'https://github.com/PowerShell/Win32-OpenSSH/wiki/Install-Win32-OpenSSH'.
+If you choose not, sshd needs read access to the private keys.
+"@    
+    Do {
+        Write-Warning $warning
+        $input = Read-Host -Prompt "Shall I continue with this insecure configuration? [Yes] Y; [No] N (default is `"Y`")"
+        if([string]::IsNullOrEmpty($input))
+        {
+            $input = 'Y'
+        }
+    } until ($input -match "^(y(es)?|N(o)?)$")
+        
+    if($Matches[0].ToLower().Startswith('y'))
+    {
+        $insecureConfig = $true
+        Write-Warning "User chose to continue with insecure configuration."
+    }
+    else
+    {        
+        Write-Warning "User chose not to continue with insecure configuration. Please register host keys with ssh-agent to have ssh remote work."
+    }
 }
+
+Get-ChildItem $PSScriptRoot\ssh*host*key* -Exclude *pub | % {
+#Get-ChildItem $PSScriptRoot\ssh_host_*_key | % {
+    if($insecureConfig) {
+        Check-FileSecure -FilePath $_.FullName -Owners $systemAccount,$adminsAccount -ReadAccessNeeded $sshdAccount
+    }
+    else {
+        Check-FileSecure -FilePath $_.FullName -Owners $systemAccount,$adminsAccount -ReadAccessOK $sshdAccount
+    }
+}
+
 
 #check public host keys
 Get-ChildItem $PSScriptRoot\ssh*host*key*.pub | % {
-    Check-FileSecure -FilePath $_.FullName -Owners $systemAccount -Identities $adminsAccount,$systemAccount -ReadOnlyPermIdentities $everyone
+#Get-ChildItem $PSScriptRoot\ssh_host_*_key.pub | % {
+    if($insecureConfig) {
+        Check-FileSecure -FilePath $_.FullName -Owners $systemAccount,$adminsAccount -ReadAccessOK $everyone -ReadAccessNeeded $sshdAccount
+    }
+    else {
+        Check-FileSecure -FilePath $_.FullName -Owners $systemAccount,$adminsAccount -ReadAccessOK $everyone
+    }
 }
 
 #check authorized_keys
 Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList" | % {
-    $userProfilePath = Get-ItemPropertyValue $_.pspath -Name ProfileImagePath -ErrorAction Ignore
-    if($userProfilePath.ToLower().Startswith("$($env:SystemDrive)\users".ToLower()))
+    $userProfilePath = Get-ItemPropertyValue $_.pspath -Name ProfileImagePath -ErrorAction Ignore    
+    $userSid = $_.PSChildName
+    $account = Get-UserSID -UserSid $userSid
+    $filePath = Join-Path $userProfilePath .ssh\authorized_keys
+    if($account -and (Test-Path $filePath))
     {
-        $userSid = $_.PSChildName
-        $account = Get-UserSID -UserSid $userSid
-        $filePath = Join-Path $userProfilePath .ssh\authorized_keys
-        if($account -and (Test-Path $filePath))
-        {
-            Check-FileSecure -FilePath $filePath -Owners $account, $adminsAccount -Identities $adminsAccount,$systemAccount,$account -ReadOnlyPermIdentities $sshdAccount
-        }
-    }        
+        Check-FileSecure -FilePath $filePath -Owners $account,$adminsAccount,$systemAccount -AnyAccessOK $account -ReadAccessNeeded $sshdAccount
+    }
 }
 
-Write-Host "--------------------Finish checking all host configs.-------------------------"  
+Write-Host "--------------------------Done-------------------------------"
