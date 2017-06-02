747cce1c7f37f099ecb8b43ff09234adb2f17dab
bingbing8 <yawang@microsoft.com>
2017-06-02 14:03:36 -0700

    added the warning back so we can enable it later

--- contrib/win32/openssh/FixHostFilePermissions.ps1
+++ contrib/win32/openssh/FixHostFilePermissions.ps1
@@ -13,6 +13,26 @@ else
 }
  
 #check private host keys
+<#$result = 'n'
+if (-not $Quiet) {
+    Do
+    {                
+        $input = Read-Host -Prompt "Did you register host private keys with ssh-agent? [Yes] Y; [No] N"    
+    } until ($input -match "^(y(es)?|N(o)?)$")
+    $result = $Matches[0]
+}
+
+if($result.ToLower().Startswith('n'))
+{
+    $warning = @"
+To keep the host private keys secure, it is recommended to register them with ssh-agent following
+steps in link https://github.com/PowerShell/Win32-OpenSSH/wiki/Install-Win32-OpenSSH.
+If you choose not to register the keys with ssh-agent, please grant sshd read access to the private host keys after run this script.
+"@
+    Write-Warning $warning
+    Write-Host " "
+}#>
+
 Get-ChildItem $PSScriptRoot\ssh_host_*_key -ErrorAction Ignore | % {    
     Fix-HostKeyPermissions -FilePath $_.FullName @psBoundParameters
 }
