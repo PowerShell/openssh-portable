9cf0d1a39784945bda3127b456f7aef53aaba203
Manoj Ampalam <manojampalam@live.com>
2017-06-21 15:02:02 -0700

    C9

--- contrib/win32/win32compat/win32_monitor_wrap.c
+++ contrib/win32/win32compat/win32_monitor_wrap.c
@@ -1,4 +1,34 @@
-
+/*
+* Author: Manoj Ampalam <manoj.ampalam@microsoft.com>
+* mm_* routines that delegate privileged operations to privileged
+* agent.
+*
+* Copyright (c) 2015 Microsoft Corp.
+* All rights reserved
+*
+* Microsoft openssh win32 port
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* 1. Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+* OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+* NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+* THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #include "includes.h"
 
@@ -53,6 +83,15 @@
 int priv_agent_sock = -1;
 int ssh_request_reply(int, struct sshbuf *, struct sshbuf *);
 
+/* 
+ * Get socket connected to privileged agent
+ * In Windows, this is implemented within ssh-agent
+ * that server both as a key-agent (like in Unix) and
+ * privileged agent. 
+ * This is a temporary accomodation until Windows has 
+ * Unix like privilege separation (monitor and less 
+ * privileged worker)
+ */
 int get_priv_agent_sock() 
 {
 	extern int auth_sock;
