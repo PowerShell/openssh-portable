2d2844cf233475b88f20f9e89f1d3b0dd05e2722
bingbing8 <yawang@microsoft.com>
2017-07-19 15:06:36 -0700

    remove one line not used.

--- contrib/win32/win32compat/w32-sshfileperm.c
+++ contrib/win32/win32compat/w32-sshfileperm.c
@@ -164,16 +164,14 @@ is_sshd_account(PSID user_sid) {
 	DWORD name_length = UNCLEN, domain_name_length = 0, full_name_len = UNCLEN + DNLEN + 2;
 	SID_NAME_USE sid_type = SidTypeInvalid;
 	BOOL ret = FALSE;
-	errno_t r = 0;
-	int t = _countof(full_name);
+	errno_t r = 0;	
 	
 	if (LookupAccountSidLocalW(user_sid, user_name, &name_length, full_name, &full_name_len, &sid_type) == FALSE)
 	{
 		debug3("LookupAccountSidLocalW() failed with error: %d. ", GetLastError());
 		errno = ENOENT;
 		return FALSE;
-	}
-	
+	}	
 	domain_name_length = wcsnlen(full_name, full_name_len);
 	full_name[domain_name_length] = L'\\';
 	if ((r = wmemcpy_s(full_name + domain_name_length + 1, full_name_len - domain_name_length -1, user_name, wcsnlen_s(user_name, UNCLEN) + 1)) != 0) {
