d4b60bf71aed1411a64b6cc7971efda0b19974dd
bingbing8 <yawang@microsoft.com>
2018-02-13 17:24:45 -0800

    update script

--- contrib/win32/openssh/OpenSSHBuildHelper.psm1
+++ contrib/win32/openssh/OpenSSHBuildHelper.psm1
@@ -236,7 +236,7 @@ function Start-OpenSSHBootstrap
         }
         elseif($errorCode -ne 0)
         {
-            Write-BuildMsg -AsError -ErrorAction Stop -Message "$packageName installation failed with error code $errorCode"
+            Write-BuildMsg -AsInfo -Message "$packageName installation failed with error code $errorCode"            
         }
     }
     else
