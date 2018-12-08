023b9a4c20fa13b6238f44da3a81ee9fed8180a8
Bryan Berns <berns@uwalumni.com>
2018-12-08 14:27:57 -0500

    Add Zlib Library For Compression Support
    - Added an empty project with a script to dynamically download zlib source and compile it into a static library.
    - Updated other projects to link with new zlib library.
    - Removed zlib preprocessor workarounds and function stubs now that real library is in place.

---
+++ contrib/win32/openssh/GetZlib.ps1
@@ -0,0 +1,52 @@
+﻿param 
+(
+    [string]$ZLibUrl = 'https://zlib.net/zlib1211.zip',
+    [string]$SourceDirectory = $null,
+    [string]$TargetLibrary = $null
+)
+
+Set-StrictMode -Version 2.0
+
+# Exit immediately if compiled zip file already exist
+If (-not [string]::IsNullOrEmpty($TargetLibrary))
+{
+    If (Test-Path -LiteralPath $TargetLibrary)
+    {
+        Exit 0
+    }
+}
+
+# Workaround that $PSScriptRoot is not support on ps version 2
+If ($PSVersiontable.PSVersion.Major -le 2)
+{
+    $PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
+}
+
+# Allow broad arrange of TLS protocols to do download
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 -bor `
+                                              [Net.SecurityProtocolType]::Tls11 -bor `
+                                              [Net.SecurityProtocolType]::Tls
+
+# Download Zlib to local directory
+$ZlibZipLocalPath = Join-Path ([System.IO.Path]::GetTempPath()) 'Zlib.zip'
+Invoke-WebRequest -Uri $ZLibUrl -OutFile $ZlibZipLocalPath
+
+
+# Set the project directory
+If ([string]::IsNullOrEmpty($SourceDirectory))
+{
+    $SourceDirectory = Join-Path $PSScriptRoot 'zlib'
+}
+
+# Ensure the destination directory exists
+New-Item -ItemType Directory $SourceDirectory -ErrorAction SilentlyContinue | Out-Null
+
+# Expand the zip file
+$ZlibTempDir = Join-Path ([System.IO.Path]::GetTempPath()) 'Zlib'
+Expand-Archive -Path $ZlibZipLocalPath -DestinationPath $ZlibTempDir -Force -ErrorAction SilentlyContinue
+$SourceDirectory = Get-Item $SourceDirectory | Select-Object -ExpandProperty FullName
+Get-ChildItem -Path "${ZlibTempDir}\*\*.c" | Copy-Item -Destination $SourceDirectory -Force
+Get-ChildItem -Path "${ZlibTempDir}\*\*.h" | Copy-Item -Destination $SourceDirectory -Force
+
+# Cleanup local zip file
+Remove-Item $ZlibZipLocalPath -Force -ErrorAction SilentlyContinue
--- sshconnect2.c
+++ sshconnect2.c
@@ -169,19 +169,14 @@ ssh_kex2(char *host, struct sockaddr *hostaddr, u_short port)
 		fatal("%s: kex_names_cat", __func__);
 	myproposal[PROPOSAL_KEX_ALGS] = compat_kex_proposal(s);
 	myproposal[PROPOSAL_ENC_ALGS_CTOS] =
-	    compat_cipher_proposal(options.ciphers);
+		compat_cipher_proposal(options.ciphers);
 	myproposal[PROPOSAL_ENC_ALGS_STOC] =
-	    compat_cipher_proposal(options.ciphers);
-#ifdef WINDOWS
-	/* compression not supported in Windows yet */
-	myproposal[PROPOSAL_COMP_ALGS_CTOS] = "none";
-#else /* !WINDOWS */
+		compat_cipher_proposal(options.ciphers);
 	myproposal[PROPOSAL_COMP_ALGS_CTOS] =
-	    myproposal[PROPOSAL_COMP_ALGS_STOC] = options.compression ?
-	    "zlib@openssh.com,zlib,none" : "none,zlib@openssh.com,zlib";
-#endif /* !WINDOWS */
+		myproposal[PROPOSAL_COMP_ALGS_STOC] = options.compression ?
+		"zlib@openssh.com,zlib,none" : "none,zlib@openssh.com,zlib";
 	myproposal[PROPOSAL_MAC_ALGS_CTOS] =
-	    myproposal[PROPOSAL_MAC_ALGS_STOC] = options.macs;
+		myproposal[PROPOSAL_MAC_ALGS_STOC] = options.macs;
 	if (options.hostkeyalgorithms != NULL) {
 		all_key = sshkey_alg_list(0, 0, 1, ',');
 		if (kex_assemble_names(&options.hostkeyalgorithms,
