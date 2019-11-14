b0396eddae32e1c07b0f59281b3ba7d332714963
Manoj Ampalam <manojamp@microsoft.com>
2019-11-14 15:12:49 -0800

    C5

--- auth.c
+++ auth.c
@@ -527,7 +527,7 @@ auth_openfile(const char *file, struct passwd *pw, int strict_modes,
                         strerror(errno));
                 return NULL;
         }
-	if (strict_modes && check_secure_file_permission(file, pw) != 0) {
+	if (strict_modes && check_secure_file_permission(file, pw, 0) != 0) {
 		fclose(f);
 		logit("Authentication refused.");
 		auth_debug_add("Ignored %s", file_type);
@@ -909,7 +909,7 @@ subprocess(const char *tag, struct passwd *pw, const char *command,
 		error("%s: inconsistent flags/output", __func__);
 		return 0;
 	}
-#ifndef  WINDOWS
+
 	/*
 	 * If executing an explicit binary, then verify the it exists
 	 * and appears safe-ish to execute
@@ -925,12 +925,20 @@ subprocess(const char *tag, struct passwd *pw, const char *command,
 		restore_uid();
 		return 0;
 	}
+#ifdef WINDOWS
+	if (check_secure_file_permission(av[0], pw, 1) != 0) {
+		error("Permissions on %s:\"%s\" are too open", tag, av[0]);
+		restore_uid();
+		return 0;
+	}
+#else
 	if (safe_path(av[0], &st, NULL, 0, errmsg, sizeof(errmsg)) != 0) {
 		error("Unsafe %s \"%s\": %s", tag, av[0], errmsg);
 		restore_uid();
 		return 0;
 	}
 #endif
+
 	/* Prepare to keep the child's stdout if requested */
 	if (pipe(p) == -1) {
 		error("%s: pipe: %s", tag, strerror(errno));
@@ -944,7 +952,6 @@ subprocess(const char *tag, struct passwd *pw, const char *command,
 		posix_spawn_file_actions_t actions;
 		pid = -1;
 
-		//TODO - run as pw user
 		if (posix_spawn_file_actions_init(&actions) != 0 ||
 			posix_spawn_file_actions_adddup2(&actions, p[1], STDOUT_FILENO) != 0)
 			fatal("posix_spawn initialization failed");
