fcb699edca9a17db4721d7828b651a88bff7e768
bagajjal <bagajjal@microsoft.com>
2019-11-25 14:38:04 -0800

    remove unnecessary code

--- contrib/win32/win32compat/win32_pty.c
+++ contrib/win32/win32compat/win32_pty.c
@@ -38,7 +38,6 @@
 int
 is_conpty_supported()
 {
-	wchar_t system32_path[PATH_MAX] = { 0, };
 	wchar_t kernel32_dll_path[PATH_MAX] = { 0, };
 	HMODULE hm_kernel32 = NULL;
 	static int isConpty = -1;
@@ -47,11 +46,6 @@ is_conpty_supported()
 		return isConpty;
 
 	isConpty = 0;
-	if (!GetSystemDirectoryW(system32_path, PATH_MAX)) {
-		error("failed to get system directory");
-		goto done;
-	}
-
 	wcscat_s(kernel32_dll_path, PATH_MAX, L"kernel32.dll");
 
 	if ((hm_kernel32 = LoadLibraryExW(kernel32_dll_path, NULL, LOAD_LIBRARY_SEARCH_SYSTEM32)) == NULL) {
