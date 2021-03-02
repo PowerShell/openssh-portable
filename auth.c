0fdc3c706898c62c53f2e9854ece8bdc22708f38
Brian Katyl <bxk@google.com>
2021-03-02 15:15:58 -0700

    Add support for AuthorizedKeysCommand and AuthorizedPrincipalsCommand to run as System
    Add support for AuthorizedKeysCommand and AuthorizedPrincipalsCommand to run as System.
    
    - When sshd is run as system and provided username is system, run command as subprocess using posix_spawnp
    - Improve debug logging in spawn_child_internal by providing different logging message for when CreateProcessW and CreateProcessAsUserW are called.
    
    This is part 2 of the a 2 part PR to Fix issue #1546

--- auth.c
+++ auth.c
@@ -955,8 +955,18 @@ subprocess(const char *tag, struct passwd *pw, const char *command,
 		if (posix_spawn_file_actions_init(&actions) != 0 ||
 			posix_spawn_file_actions_adddup2(&actions, p[1], STDOUT_FILENO) != 0)
 			fatal("posix_spawn initialization failed");
-		else if (__posix_spawn_asuser((pid_t*)&pid, av[0], &actions, NULL, av, NULL, pw->pw_name) != 0)
-			fatal("posix_spawn: %s", strerror(errno));
+		else {
+			if (strcmp(pw->pw_name, "system") == 0 && am_system()) {
+				debug("starting subprocess using posix_spawnp");
+				if (posix_spawnp((pid_t*)&pid, av[0], &actions, NULL, av, NULL) != 0)
+					fatal("posix_spawnp: %s", strerror(errno));
+			}
+			else {
+				debug("starting subprocess as user using __posix_spawn_asuser");
+				if (__posix_spawn_asuser((pid_t*)&pid, av[0], &actions, NULL, av, NULL, pw->pw_name) != 0)
+					fatal("posix_spawn_user: %s", strerror(errno));
+			}
+		}
 
 		posix_spawn_file_actions_destroy(&actions);
 	}
