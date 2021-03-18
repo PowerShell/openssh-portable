13fe2a463dfa8786aa25080ffd3352e3d8e8c317
bagajjal <bagajjal@microsoft.com>
2021-03-18 16:19:54 -0700

    uncomment the restart ssh-agent code to debug further

--- contrib/win32/openssh/OpenSSHTestHelper.psm1
+++ contrib/win32/openssh/OpenSSHTestHelper.psm1
@@ -206,13 +206,13 @@ WARNING: Following changes will be made to OpenSSH configuration
         }
     }
 
-    #restart ssh-agent
-    Write-Host "ssh-agent service state before restarting"
-    Get-Service ssh-agent | Format-List *
-    Stop-Service ssh-agent
-    Start-Service ssh-agent
-    Write-Host "Restarted ssh-agent before setting up single sing on user"
-    Get-Service ssh-agent | Format-List *
+    # #restart ssh-agent
+    # Write-Host "ssh-agent service state before restarting"
+    # Get-Service ssh-agent | Format-List *
+    # Stop-Service ssh-agent
+    # Start-Service ssh-agent
+    # Write-Host "Restarted ssh-agent before setting up single sing on user"
+    # Get-Service ssh-agent | Format-List *
 
     #setup single sign on for ssouser
     $ssouserProfile = Get-LocalUserProfile -User $SSOUser
