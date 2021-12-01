1e3d6d4ac625280e17aa6807eb9d27b71b789080
pedro martelletto <pedro@yubico.com>
2021-12-01 09:53:03 +0100

    sk-usbhid: +#ifdef WINDOWS; requested by bagajjal@

--- sk-usbhid.c
+++ sk-usbhid.c
@@ -686,7 +686,11 @@ sk_enroll(uint32_t alg, const uint8_t *challenge, size_t challenge_len,
 		goto out;
 	}
 	*enroll_response = NULL;
+#ifdef WINDOWS
 	arc4random_buf(user_id, sizeof(user_id));
+#else
+	memset(user_id, 0, sizeof(user_id));
+#endif
 	if (check_enroll_options(options, &device, user_id,
 	    sizeof(user_id)) != 0)
 		goto out; /* error already logged */
