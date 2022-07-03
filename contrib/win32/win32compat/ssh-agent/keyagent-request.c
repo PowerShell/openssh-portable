f1b20646e68b3edffa2fda739a9e7aad5f6becfb
Dan Drown <dan-android@drown.org>
2022-07-03 17:52:43 -0500

    parse agent constraint messages
    returns SSH_AGENT_FAILURE on unsupported constraint types, such as:
    * SSH_AGENT_CONSTRAIN_LIFETIME
    * SSH_AGENT_CONSTRAIN_CONFIRM
    * SSH_AGENT_CONSTRAIN_MAXSIGN
    
    returns SSH_AGENT_FAILURE on unsupported constrain extensions, such as:
    "restrict-destination-v00@openssh.com"
    
    accepts and ignores constrain extension "sk-provider@openssh.com"

--- contrib/win32/win32compat/ssh-agent/keyagent-request.c
+++ contrib/win32/win32compat/ssh-agent/keyagent-request.c
@@ -224,6 +224,59 @@ process_unsupported_request(struct sshbuf* request, struct sshbuf* response, str
 	return r;
 }
 
+static int
+parse_key_constraint_extension(struct sshbuf *m)
+{
+	char *ext_name = NULL;
+	int r;
+
+	if ((r = sshbuf_get_cstring(m, &ext_name, NULL)) != 0) {
+		error_fr(r, "parse constraint extension");
+		goto out;
+	}
+	debug_f("constraint ext %s", ext_name);
+	if (strcmp(ext_name, "sk-provider@openssh.com") == 0) {
+		if ((r = sshbuf_skip_string(m)) != 0) {
+			error_fr(r, "parse %s", ext_name);
+			goto out;
+		}
+	} else {
+		error_f("unsupported constraint \"%s\"", ext_name);
+		r = SSH_ERR_FEATURE_UNSUPPORTED;
+		goto out;
+	}
+	/* success */
+	r = 0;
+ out:
+	free(ext_name);
+	return r;
+}
+
+static int
+parse_key_constraints(struct sshbuf *m)
+{
+	int r;
+	u_char ctype;
+
+	while (sshbuf_len(m)) {
+		if ((r = sshbuf_get_u8(m, &ctype)) != 0) {
+			error("get constraint type returned %d", r);
+			return r;
+		}
+		switch (ctype) {
+		case SSH_AGENT_CONSTRAIN_EXTENSION:
+			if ((r = parse_key_constraint_extension(m)) != 0)
+				return r;
+			break;
+		default:
+			error("Unknown constraint %d", ctype);
+			return SSH_ERR_FEATURE_UNSUPPORTED;
+		}
+	}
+
+	return 0;
+}
+
 int
 process_add_identity(struct sshbuf* request, struct sshbuf* response, struct agent_connection* con) 
 {
@@ -245,6 +298,12 @@ process_add_identity(struct sshbuf* request, struct sshbuf* response, struct age
 	    sshbuf_get_cstring(request, &comment, &comment_len) != 0) {
 		debug("key add request is invalid");
 		request_invalid = 1;
+		goto done;
+	}
+
+	if ((r = parse_key_constraints(request)) != 0) {
+		if (r != SSH_ERR_FEATURE_UNSUPPORTED)
+			request_invalid = 1;
 		goto done;
 	}
 
