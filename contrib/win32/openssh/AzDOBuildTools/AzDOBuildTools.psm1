8a46b421ce549b0933e0caf7d58da3a3ce282e92
tgauth@bu.edu <tessgauthier@microsoft.com>
2023-11-16 11:39:40 -0500

    temp build in debug

--- contrib/win32/openssh/AzDOBuildTools/AzDOBuildTools.psm1
+++ contrib/win32/openssh/AzDOBuildTools/AzDOBuildTools.psm1
@@ -35,8 +35,9 @@ function Write-BuildMessage
 #>
 function Invoke-AzDOBuild
 {
-      Start-OpenSSHBuild -Configuration Release -NativeHostArch x64 -Verbose
-      Start-OpenSSHBuild -Configuration Release -NativeHostArch x86 -Verbose
+      # temporary change to build debug instead of release because of zlib different compiler version error
+      Start-OpenSSHBuild -Configuration Debug -NativeHostArch x64 -Verbose
+      Start-OpenSSHBuild -Configuration Debug -NativeHostArch x86 -Verbose
       Write-BuildMessage -Message "OpenSSH binaries build success!" -Category Information
 }
 
