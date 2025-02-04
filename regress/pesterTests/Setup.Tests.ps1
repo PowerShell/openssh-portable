101475d0dbf96e0fa98ca6e7e23ec3f57fa0b411
Tess Gauthier <tessgauthier@microsoft.com>
2025-02-04 14:11:51 -0500

    Update Setup.Tests.ps1

--- regress/pesterTests/Setup.Tests.ps1
+++ regress/pesterTests/Setup.Tests.ps1
@@ -535,6 +535,9 @@ Describe "Setup Tests" -Tags "Setup" {
             if (Test-Path -Path $logFolderPath) {
                 $logACL = Get-Acl $logFolderPath
             }
+            if ((Get-Service sshd).Status -eq 'Running') {
+                net stop sshd
+            }
         }
         AfterAll {
             $tC++
