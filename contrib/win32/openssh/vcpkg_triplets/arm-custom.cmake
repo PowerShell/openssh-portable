fd3848746799b5ca289c37750f41926afa50a108
Tess Gauthier <tessgauthier@microsoft.com>
2025-03-04 17:03:18 -0500

    update custom triplet naming in proj files

--- contrib/win32/openssh/vcpkg_triplets/arm-custom.cmake
+++ contrib/win32/openssh/vcpkg_triplets/arm-custom.cmake
@@ -1,9 +1,9 @@
 set(VCPKG_TARGET_ARCHITECTURE arm)
 
-if(${PORT} MATCHES "libressl")
-	set(VCPKG_CRT_LINKAGE dynamic)
-	set(VCPKG_LIBRARY_LINKAGE dynamic)
-else()
+if(${PORT} MATCHES "zlib")
 	set(VCPKG_CRT_LINKAGE static)
 	set(VCPKG_LIBRARY_LINKAGE static)
+else()
+	set(VCPKG_CRT_LINKAGE dynamic)
+	set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
