2bc16b6af804e792ae00acc0c99588c5550e8801
Dmitry Sapozhnikov <11535558+o-sdn-o@users.noreply.github.com>
2025-11-05 18:37:30 +0500

    Reformat the if block

--- contrib/win32/win32compat/signal.c
+++ contrib/win32/win32compat/signal.c
@@ -313,8 +313,8 @@ wait_for_any_event(HANDLE* events, int num_events, DWORD milli_seconds)
 void
 terminate_all_child_processes()
 {
-	if (children.num_children > 0 && children.num_children <= MAX_CHILDREN && children.num_children > children.num_zombies) {
-		if (children.num_zombies >= 0) {
+	if (children.num_children > 0 && children.num_children <= MAX_CHILDREN) {
+		if (children.num_zombies >= 0 && children.num_children > children.num_zombies) {
 			DWORD live_children = children.num_children - children.num_zombies;
 			while (live_children--) {
 				DWORD pid = children.process_id[live_children];
