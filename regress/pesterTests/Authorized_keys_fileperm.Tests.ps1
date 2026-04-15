c9f2480b07a985fc3302b02723d3bfe60221af22
Tess Gauthier <tessgauthier@microsoft.com>
2026-04-15 15:49:48 -0400

    pin cygwin version as test

--- regress/pesterTests/Authorized_keys_fileperm.Tests.ps1
+++ regress/pesterTests/Authorized_keys_fileperm.Tests.ps1
@@ -193,12 +193,10 @@ Describe "Tests for authorized_keys file permission" -Tags "CI" {
         }
 
         It "$tC.$tI-authorized_keys-negative(other account has ChangePermissions on authorized_keys file)"  -skip:$skip {
-            #setup clean ACL then grant PwdUser ChangePermissions (WRITE_DAC)
             Repair-FilePermission -Filepath $authorizedkeyPath -Owner $objUserSid -FullAccessNeeded $adminsSid,$systemSid,$objUserSid -confirm:$false
             $objPwdUserSid = Get-UserSid -User $PwdUser
             Set-FilePermission -FilePath $authorizedkeyPath -User $objPwdUserSid -Perm "ChangePermissions"
 
-            #Run
             Start-SSHDTestDaemon -workDir $opensshbinpath -Arguments "-d -f $sshdconfig -o `"AuthorizedKeysFile .testssh/authorized_keys`" -E $sshdlog" -Port $port
             ssh -p $port -E $sshlog $ssouser@$server echo 1234
             $LASTEXITCODE | Should Not Be 0
@@ -209,12 +207,10 @@ Describe "Tests for authorized_keys file permission" -Tags "CI" {
         }
 
         It "$tC.$tI-authorized_keys-negative(other account has TakeOwnership on authorized_keys file)"  -skip:$skip {
-            #setup clean ACL then grant PwdUser TakeOwnership (WRITE_OWNER)
             Repair-FilePermission -Filepath $authorizedkeyPath -Owner $objUserSid -FullAccessNeeded $adminsSid,$systemSid,$objUserSid -confirm:$false
             $objPwdUserSid = Get-UserSid -User $PwdUser
             Set-FilePermission -FilePath $authorizedkeyPath -User $objPwdUserSid -Perm "TakeOwnership"
 
-            #Run
             Start-SSHDTestDaemon -workDir $opensshbinpath -Arguments "-d -f $sshdconfig -o `"AuthorizedKeysFile .testssh/authorized_keys`" -E $sshdlog" -Port $port
             ssh -p $port -E $sshlog $ssouser@$server echo 1234
             $LASTEXITCODE | Should Not Be 0
@@ -225,12 +221,10 @@ Describe "Tests for authorized_keys file permission" -Tags "CI" {
         }
 
         It "$tC.$tI-authorized_keys-negative(other account has Delete on authorized_keys file)"  -skip:$skip {
-            #setup clean ACL then grant PwdUser Delete (DELETE)
             Repair-FilePermission -Filepath $authorizedkeyPath -Owner $objUserSid -FullAccessNeeded $adminsSid,$systemSid,$objUserSid -confirm:$false
             $objPwdUserSid = Get-UserSid -User $PwdUser
             Set-FilePermission -FilePath $authorizedkeyPath -User $objPwdUserSid -Perm "Delete"
 
-            #Run
             Start-SSHDTestDaemon -workDir $opensshbinpath -Arguments "-d -f $sshdconfig -o `"AuthorizedKeysFile .testssh/authorized_keys`" -E $sshdlog" -Port $port
             ssh -p $port -E $sshlog $ssouser@$server echo 1234
             $LASTEXITCODE | Should Not Be 0
@@ -245,7 +239,6 @@ Describe "Tests for authorized_keys file permission" -Tags "CI" {
             $objPwdUserSid = Get-UserSid -User $PwdUser
             Repair-FilePermission -Filepath $authorizedkeyPath -Owner $objPwdUserSid -FullAccessNeeded $adminsSid,$systemSid,$objPwdUser -confirm:$false
 
-            #Run
             Start-SSHDTestDaemon -WorkDir $opensshbinpath -Arguments "-d -f $sshdconfig -o `"AuthorizedKeysFile .testssh/authorized_keys`" -E $sshdlog" -Port $port
             ssh -p $port -E $sshlog $ssouser@$server echo 1234
             $LASTEXITCODE | Should Not Be 0
